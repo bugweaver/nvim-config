1969a879f4c419cb17fdd5eb87e22724db66d075
bugweaver <illustrati0@yandex.ru>
2025-09-01 22:44:08 +0300

    style: reformat

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -2,13 +2,13 @@
 vim.g.mapleader = " "
 
 -- Insert
-vim.keymap.set('i', 'jj', '<Esc>')
+vim.keymap.set("i", "jj", "<Esc>")
 
 -- Buffers
-vim.keymap.set('n', '<leader>w', ':w<CR>')
+vim.keymap.set("n", "<leader>w", ":w<CR>")
 
 -- Neo-tree
-vim.keymap.set('n', '<leader>e', ':Neotree toggle reveal<CR>')
+vim.keymap.set("n", "<leader>e", ":Neotree toggle reveal<CR>")
 
 -- Explorer (Snacks)
 --vim.keymap.set('n', '<leader>e', function()
@@ -16,17 +16,17 @@ vim.keymap.set('n', '<leader>e', ':Neotree toggle reveal<CR>')
 --end, { desc = 'Snacks Explorer' })
 
 -- Navigation
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
 -- Splits
-vim.keymap.set('n', '|', ':vsplit<CR>')
-vim.keymap.set('n', '\\', ':split<CR>')
+vim.keymap.set("n", "|", ":vsplit<CR>")
+vim.keymap.set("n", "\\", ":split<CR>")
 
 -- Tabs
-vim.keymap.set('n', '<Tab>', ':BufferLineCycleNext<CR>')
-vim.keymap.set('n', '<s-Tab>', ':BufferLineCyclePrev<CR>')
-vim.keymap.set('n', '<leader>x', ':BufferLinePickClose<CR>')
-vim.keymap.set('n', '<c-x>', ':BufferLineCloseOthers<CR>')
+vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>")
+vim.keymap.set("n", "<s-Tab>", ":BufferLineCyclePrev<CR>")
+vim.keymap.set("n", "<leader>x", ":BufferLinePickClose<CR>")
+vim.keymap.set("n", "<c-x>", ":BufferLineCloseOthers<CR>")
